828ed450a9502cd3dd3cc5968483ec02043d36e4
Cesar Ceron <ceron.cmcr@gmail.com>
2022-09-25 16:09:13 -0600

    commit initial

---
+++ Ejercicio7/Ejercicio7.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio7.aspx.cs" Inherits="Ejercicio7.Ejercicio7" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" style="display:flex;justify-content:center;">
+        <div style="width:250px;margin:0 0 0 1rem">
+            <asp:Panel ID="Panel1" runat="server"></asp:Panel>            
+        </div>
+        <div style="width:50%;margin:10px 0 0 50px">
+            <asp:Label ID="LblData" runat="server" Text="Label" Visible="False"></asp:Label>
+        </div>        
+    </form>    
+</body>
+</html>
---
+++ Ejercicio7/Ejercicio7.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Web.UI.WebControls;
+
+namespace Ejercicio7
+{
+    public partial class Ejercicio7 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Button button;
+            for (int i = 1; i <= 100; i++)
+            {
+                button = new Button();
+                button.ID = $"Btn{i}";
+                button.Width = 50;
+                button.Text = i.ToString();
+                button.BackColor = Color.AliceBlue;
+                button.Click += new EventHandler(button_Click);
+                Panel1.Controls.Add(button);
+            }                                
+        }
+
+        protected void button_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender; ;
+            int num = int.Parse(btn.Text);
+            this.LblData.Visible = true;
+            this.LblData.Text = GenerarTabla(num);
+        }
+
+        private string GenerarTabla(int numero)
+        {
+            string tabla = "";
+            for (int i = 1; i <= 10; i++)
+            {
+                tabla += $"{numero} x {i} = {numero * i}</br>";
+            }
+            return tabla;
+        }
+    }
+}
